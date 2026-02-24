999948c489c1ad3e3577c8f20df02713eb94145b
NightmareOkami <dazaklena@gmail.com>
2026-02-24 02:44:47 +0000

    Fixing Java syntax and path

---
+++ src/main/java/com/neu/NotEnoughUpdatesFabric.java
@@ -0,0 +1,10 @@
+package com.neu;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NotEnoughUpdatesFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("NEU Fabric Initialized!");
+    }
+}
